a492d98e4cbcadcd8306669204619401b7248318
brettmcc <brettmcc@users.noreply.github.com>
2016-09-14 10:28:56 -0700

    Update runpsid.bat
    Added log rerouting and stat-transfer commands

--- build/code/runpsid.bat
+++ build/code/runpsid.bat
@@ -1,5 +1,11 @@
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\pter-master\pter-master\build\code\HourCode\WantAble.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\pter-master\pter-master\build\code\HourCode\Food.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\pter-master\pter-master\build\code\HourCode\Housework.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "W:\pter-master\pter-master\build\code\HourCode\PSIDMerge.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg"
-"C:\Program Files (x86)\Stata14\Stata-64.exe" /b do W:\pter-master\pter-master\build\code\format_pooled_psid.do
+del /Q F:\pter-master\build\temp\*	
+
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "F:\pter-master\build\code\HourCode\WantAble.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg" -LOG "F:\pter-master\build\temp\WantAble.log"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "F:\pter-master\build\code\HourCode\Food.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg" -LOG "F:\pter-master\build\temp\Food.log"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "F:\pter-master\build\code\HourCode\Housework.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg" -LOG "F:\pter-master\build\temp\Housework.log"
+"C:\Program Files\SASHome\SASFoundation\9.4\Sas.exe" -SYSIN "F:\pter-master\build\code\HourCode\PSIDMerge.sas" -CONFIG "C:\Program Files\SASHome\SASFoundation\9.4\nls\u8\sasv9.cfg" -LOG "F:\pter-master\build\temp\PSIDMerge.log"
+
+"C:\Program Files\StatTransfer11-64\st.exe" "F:\pter-master\build\output\pooled.sas7bdat" "F:\pter-master\build\output\pooled.dta" -y
+
+cd F:\pter-master\build\temp\
+"C:\Program Files (x86)\Stata14\Stata-64.exe" /e do F:\pter-master\build\code\format_pooled_psid.do
